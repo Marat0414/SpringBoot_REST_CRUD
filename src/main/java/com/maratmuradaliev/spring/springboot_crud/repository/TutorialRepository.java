c1c2ab19e2d780033117cc0789dcf416c7f527da
Marat Muradaliev <marat.muradaliev0414@gmail.com>
2023-06-29 12:01:47 +0600

    Initial commit

---
+++ src/main/java/com/maratmuradaliev/spring/springboot_crud/repository/TutorialRepository.java
@@ -0,0 +1,14 @@
+package com.maratmuradaliev.spring.springboot_crud.repository;
+
+import com.maratmuradaliev.spring.springboot_crud.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial,Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
+
+
+}
+
